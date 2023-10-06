2e469afcb51aad59a1e634499fd593e054044cc1
Antonio Trindade <atrindade@umov.me>
2023-10-05 21:19:46 -0300

    Cria testes datadriven

--- src/test/java/com/core/BaseTest.java
+++ src/test/java/com/core/BaseTest.java
@@ -4,6 +4,7 @@ import static com.core.DriverFactory.getDriver;
 import static com.core.DriverFactory.killDriver;
 
 import java.io.File;
+import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -16,11 +17,13 @@ public abstract class BaseTest {
 	 
 	@AfterMethod
 	public void tearDown(ITestResult testResult) throws Exception {
+		Date date = new Date();
+		
 		//screenshot da tela
 		File screen = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
 		//salva o arquivo de screenshot na pasta
-		FileUtils.copyFile(screen, new File("target" + File.separator + testResult.getName()+".jpg"));
+		FileUtils.copyFile(screen, new File("target" + File.separator + testResult.getName()+"_"+date+".jpg"));
+		
 		killDriver();
 	}	
-
 }
--- src/test/java/com/test/SearchCountriesTest.java
+++ src/test/java/com/test/SearchCountriesTest.java
@@ -2,11 +2,13 @@ package com.test;
 
 import static org.testng.Assert.assertEquals;
 
-import org.testng.annotations.BeforeTest;
+import java.io.File;
+
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.core.BaseTest;
+import com.core.SpreadsheetData;
 import com.page.DetailPage;
 import com.page.MainPage;
 
@@ -37,5 +39,21 @@ public class SearchCountriesTest extends BaseTest{
 		};				
 	}
 	
+	@Test(dataProvider="countriesListExcel")
+	public void searchCountryExcel(String searchCountry, String expectedCountry) {	
+		mainPage = new MainPage();
+		mainPage.open();		
+		detailPage = mainPage.search(searchCountry);		
+		assertEquals(detailPage.getTextTitle(), expectedCountry);			
+	}
+	
+	@DataProvider(name="countriesListExcel")
+	public Object[][] dataProviderExcel(){
+		Object[][] testData = SpreadsheetData.readExcelData(
+				"Paises", 
+				"src" +File.separator + "test" + File.separator + "resources" + File.separator+ "paises.xls", 
+				"Dados");
+		return testData;
+	}
 
 }
